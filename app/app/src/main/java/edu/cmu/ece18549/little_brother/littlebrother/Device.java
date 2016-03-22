c7a6c711bc0b720d48684639566b2a666d512699
rnatour <rnatour@andrew.cmu.edu>
2016-03-22 03:58:26 -0400

    added models
    Added models for Devices, Sensors, and Logs

--- app/app/src/main/java/edu/cmu/ece18549/little_brother/littlebrother/Device.java
+++ app/app/src/main/java/edu/cmu/ece18549/little_brother/littlebrother/Device.java
@@ -1,19 +1,132 @@
 package edu.cmu.ece18549.little_brother.littlebrother;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Ramsey on 3/20/2016.
  */
 public class Device {
     private final int mId;
-    public Device(int id) {
+    private String mName;
+    private double mLatitude;
+    private double mLongitude;
+    private Map<Sensor,List<DeviceLog>> mComponents;
+
+    public Device(int id, String name, double latitude, double longitude) {
+
         mId = id;
+        mName = name;
+        mLatitude = latitude;
+        mLongitude = longitude;
+        mComponents = new HashMap<Sensor,List<DeviceLog>>();
     }
 
     public String getName() {
-        return mId+"";
+        return mName;
     }
 
-    public String getId() {
-        return mId+"";
+    public void setName(String name) {
+        mName = name;
     }
+
+    public int getId() {
+        return mId;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public void setLongitude(float longitude) {
+        mLongitude = longitude;
+    }
+
+    public void setLatitude(float latitude) {
+        mLatitude = latitude;
+    }
+
+    public synchronized void addLog(DeviceLog log) throws DeviceException {
+        Sensor sensor = log.getSensor();
+        if (!mComponents.containsKey(sensor)) {
+            throw new DeviceException("Sensor " + sensor +" not found");
+        } else {
+            List<DeviceLog> logs = mComponents.get(sensor);
+            logs.add(log);
+        }
+    }
+
+    public synchronized void removeLog(DeviceLog log, Sensor sensor) throws DeviceException {
+        if (!mComponents.containsKey(sensor)) {
+            throw new DeviceException("Sensor " + sensor +" not found");
+        } else {
+            List<DeviceLog> logs = mComponents.get(sensor);
+            if (!logs.contains(log)) {
+                throw new DeviceException("Log " + log + " not found");
+            } else {
+                logs.remove(log);
+            }
+        }
+    }
+
+    public synchronized ArrayList<DeviceLog> getLogs(Sensor sensor) throws DeviceException {
+        if (!mComponents.containsKey(sensor)) {
+            throw new DeviceException("Sensor " + sensor +" not found");
+        } else {
+            return new ArrayList<DeviceLog>(mComponents.get(sensor));
+        }
+    }
+
+    public synchronized ArrayList<DeviceLog> getLogs() {
+        ArrayList<DeviceLog> newLogs = new ArrayList<DeviceLog>();
+        Collection<List<DeviceLog>> allLogs = mComponents.values();
+        for(List<DeviceLog> list : allLogs) {
+            for(DeviceLog log : list) {
+                newLogs.add(log);
+            }
+        }
+
+        return newLogs;
+    }
+
+    public synchronized ArrayList<Sensor> getSensors() {
+        return new ArrayList<Sensor>(mComponents.keySet());
+    }
+
+    public synchronized void addSensor(Sensor sensor) throws DeviceException {
+        if (mComponents.containsKey(sensor)) {
+            throw new DeviceException("Sensor " + sensor + " already on device");
+        }
+
+        List<DeviceLog> newLogs = new LinkedList<DeviceLog>();
+        mComponents.put(sensor,newLogs);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Device)) {
+            return false;
+        }
+        Device o1 = (Device) object;
+        return o1.mId == mId;
+    }
+
+    @Override
+    public int hashCode() {
+        return mId;
+    }
+
+    @Override
+    public String toString() {
+        return "" + mId;
+    }
+
 }
---
+++ app/app/src/main/java/edu/cmu/ece18549/little_brother/littlebrother/DeviceLog.java
@@ -0,0 +1,66 @@
+package edu.cmu.ece18549.little_brother.littlebrother;
+
+import java.util.Date;
+
+/**
+ * Created by Ramsey on 3/21/2016.
+ */
+public class DeviceLog {
+
+    private final int mId;
+    private final Date mDate;
+    private final double mValue;
+    private final Date mTimeRecieved;
+    private final Sensor mSensor;
+
+    public DeviceLog(int id, Date date, double value, Date timeRecieved, Sensor sensor) {
+        mId = id;
+        mDate = date;
+        mValue = value;
+        mTimeRecieved = timeRecieved;
+        mSensor = sensor;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public double getValue() {
+        return mValue;
+    }
+
+    public Date getTimeRecieved() {
+        return mTimeRecieved;
+    }
+
+    public Sensor getSensor() {
+        return mSensor;
+    }
+
+    public Device getDevice() {
+        return mSensor.getDevice();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof DeviceLog)) {
+            return false;
+        }
+        DeviceLog o1 = (DeviceLog) object;
+        return o1.mId == mId && o1.mSensor.equals(mSensor);
+    }
+
+    @Override
+    public int hashCode() {
+        return mId;
+    }
+
+    @Override
+    public String toString() {
+        return ""+mId;
+    }
+}
